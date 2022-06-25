a4bbfbfe3cc9be4d9b86cf3068f5b30f45eba4f9
kiccer <1072907338@qq.com>
2022-06-25 09:02:17 +0800

    🦄 refactor: 调整代码位置，修改生效时机，阻止出现原样式一闪而逝的情况。

--- scripts/greasyfork_beautify.js
+++ scripts/greasyfork_beautify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greasyfork Beautify
 // @namespace    https://github.com/kiccer
-// @version      0.12.alpha
+// @version      0.13.alpha
 // @description  自然、优雅，仿若本该如此...
 // @author       kiccer<1072907338@qq.com>
 // @supportURL   https://github.com/kiccer/TampermonkeyScripts/issues
@@ -27,304 +27,305 @@ if (!new RegExp(`/${navigator.language}/?`).test(location.href)) {
     location.href = location.href.replace(/^(https:\/\/greasyfork\.org\/)[a-zA-Z-]+(\/?.*)/, `$1${navigator.language}$2`)
 }
 
-window.addEventListener('load', () => {
-    // 样式初始化
-    GM_addStyle(GM_getResourceText('normalize.css'))
-    // eslint-disable-next-line no-eval
-    eval(GM_getResourceText('less'))
+// eslint-disable-next-line no-eval
+eval(GM_getResourceText('less'))
 
-    const lessOptions = {}
+const lessOptions = {}
 
-    const lessInput = `
-        // --------------------------------------------- 变量
+const lessInput = `
+    // --------------------------------------------- 变量
 
-        @nav_height: 60px;
-        @user_container_height: 24px;
+    @nav_height: 60px;
+    @user_container_height: 24px;
 
-        // --------------------------------------------- 混合宏
+    // --------------------------------------------- 混合宏
 
-        .ellipsis (@lines) {
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            overflow: hidden;
-            line-height: 1.5;
-            -webkit-line-clamp: @lines;
-        }
+    .ellipsis (@lines) {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        line-height: 1.5;
+        -webkit-line-clamp: @lines;
+    }
 
-        // --------------------------------------------- 通用样式
+    // --------------------------------------------- 通用样式
 
-        * {
-            box-sizing: border-box;
-            outline: none;
-        }
+    * {
+        box-sizing: border-box;
+        outline: none;
+    }
 
-        body {
-            line-height: 1.5;
+    body {
+        line-height: 1.5;
+        min-height: 100vh;
+        background-color: #f7f7f7;
+
+        > .width-constraint {
             min-height: 100vh;
-            background-color: #f7f7f7;
-
-            > .width-constraint {
-                min-height: 100vh;
-                background-color: #fff;
-                padding: 20px;
-                padding-top: calc(@nav_height + @user_container_height + 20px);
-                
-                .text-content {
-                    border: 0;
-                    box-shadow: none;
-                    padding: 0;
-                }
-            }
-        }
-
-        a {
-            color: rgb(38, 38, 38);
-            text-decoration: none;
-
-            &:hover {
-                text-decoration: underline;
-            }
-
-            &:visited {
-                color: rgb(38, 38, 38);
-            }
-        }
-
-        pre.lang-js {
-            font-family: Consolas;
-
-            ol.linenums {
-                li.L1, li.L3, li.L5, li.L7, li.L9  {
-                    background-color: rgba(0, 0, 0, .02);
-                }
-            }
-        }
-
-        .pagination {
-            margin-top: 20px !important;
-            user-select: none;
-
-            > * {
-                padding: 0 .5em !important;
-                min-width: 2em;
-                height: 2em;
-                line-height: 2;
-                text-align: center;
-                text-decoration: none !important;
-            }
-        }
-
-        // --------------------------------------------- 输入框
-
-        input[type=search] {
-            padding: 3px 6px;
-            padding-right: 2.4em !important;
-            border: 1px solid #bfbfbf;
-            border-radius: 4px;
-        }
-
-        form {
-            input.search-submit {
-                top: 50% !important;
-                transform: translateY(-50%);
-                cursor: pointer;
-            }
-        }
-
-        .home-search {
-            margin-bottom: 20px;
-        }
-
-        .sidebar-search {
-            margin-bottom: 20px;
-
-            input[type="search"] {
-                margin: 0;
-            }
-        }
-
-        // --------------------------------------------- header
-
-        #main-header {
-            background-color: #000;
-            background-image: none;
-            width: 100%;
-            padding: 0;
-            position: fixed;
-            top: 0;
-            z-index: 1;
-            user-select: none;
-            box-shadow: 0 0 5px 2px rgb(0 0 0 / 50%);
-
-            .width-constraint {
-                display: flex;
-                justify-content: space-between;
-                height: 100%;
+            background-color: #fff;
+            padding: 20px;
+            padding-top: calc(@nav_height + @user_container_height + 20px);
+            
+            .text-content {
+                border: 0;
+                box-shadow: none;
                 padding: 0;
+            }
+        }
+    }
 
-                #site-name {
-                    display: flex;
-                    align-items: center;
-                    
-                    a {
-                        display: block;
-                    }
+    a {
+        color: rgb(38, 38, 38);
+        text-decoration: none;
 
-                    img {
-                        width: auto;
-                        height: 50px;
-                    }
+        &:hover {
+            text-decoration: underline;
+        }
 
-                    #site-name-text {
-                        margin-left: 10px;
+        &:visited {
+            color: rgb(38, 38, 38);
+        }
+    }
 
-                        h1 {
-                            font-size: 36px;
-                            display: flex;
-                            display: flex;
-                            align-items: baseline;
+    pre.lang-js {
+        font-family: Consolas;
 
-                            &::after {
-                                content: "Greasyfork Beautify V${VERSION}";
-                                font-size: 12px;
-                                letter-spacing: 1px;
-                                font-family: "微软雅黑";
-                                font-weight: 200;
-                                color: rgba(255, 255, 255, .5);
-                                line-height: 1;
-                                margin-left: 10px;
-                            }
+        ol.linenums {
+            li.L1, li.L3, li.L5, li.L7, li.L9  {
+                background-color: rgba(0, 0, 0, .02);
+            }
+        }
+    }
+
+    .pagination {
+        margin-top: 20px !important;
+        user-select: none;
+
+        > * {
+            padding: 0 .5em !important;
+            min-width: 2em;
+            height: 2em;
+            line-height: 2;
+            text-align: center;
+            text-decoration: none !important;
+        }
+    }
+
+    // --------------------------------------------- 输入框
+
+    input[type=search] {
+        padding: 3px 6px;
+        padding-right: 2.4em !important;
+        border: 1px solid #bfbfbf;
+        border-radius: 4px;
+    }
+
+    form {
+        input.search-submit {
+            top: 50% !important;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
+    }
+
+    .home-search {
+        margin-bottom: 20px;
+    }
+
+    .sidebar-search {
+        margin-bottom: 20px;
+
+        input[type="search"] {
+            margin: 0;
+        }
+    }
+
+    // --------------------------------------------- header
+
+    #main-header {
+        background-color: #000;
+        background-image: none;
+        width: 100%;
+        padding: 0;
+        position: fixed;
+        top: 0;
+        z-index: 1;
+        user-select: none;
+        box-shadow: 0 0 5px 2px rgb(0 0 0 / 50%);
+
+        .width-constraint {
+            display: flex;
+            justify-content: space-between;
+            height: 100%;
+            padding: 0;
+
+            #site-name {
+                display: flex;
+                align-items: center;
+                
+                a {
+                    display: block;
+                }
+
+                img {
+                    width: auto;
+                    height: 50px;
+                }
+
+                #site-name-text {
+                    margin-left: 10px;
+
+                    h1 {
+                        font-size: 36px;
+                        display: flex;
+                        display: flex;
+                        align-items: baseline;
+
+                        &::after {
+                            content: "Greasyfork Beautify V${VERSION}";
+                            font-size: 12px;
+                            letter-spacing: 1px;
+                            font-family: "微软雅黑";
+                            font-weight: 200;
+                            color: rgba(255, 255, 255, .5);
+                            line-height: 1;
+                            margin-left: 10px;
                         }
                     }
                 }
             }
+        }
 
-            #user-container {
-                width: 100%;
+        #user-container {
+            width: 100%;
+            height: @user_container_height;
+            background-color: #343434;
+
+            .user-main {
+                margin: auto;
+                max-width: 1200px;
                 height: @user_container_height;
-                background-color: #343434;
+                line-height: @user_container_height;
+                text-align: right;
+                font-size: 14px;
+                padding-right: 10px;
 
-                .user-main {
-                    margin: auto;
-                    max-width: 1200px;
-                    height: @user_container_height;
-                    line-height: @user_container_height;
-                    text-align: right;
-                    font-size: 14px;
-                    padding-right: 10px;
-
-                    @media screen and (max-width: 1228px) {
-                        margin: auto 1.2vw;
-                    }
+                @media screen and (max-width: 1228px) {
+                    margin: auto 1.2vw;
                 }
             }
         }
+    }
 
-        #site-nav {
-            width: 0;
-            height: 0;
-            border: 0;
-            padding: 0;
-            overflow: hidden;
+    #site-nav {
+        width: 0;
+        height: 0;
+        border: 0;
+        padding: 0;
+        overflow: hidden;
+        position: relative;
+    }
+
+    #site-nav-vue {
+        display: flex;
+        
+        .nav-item {
+            line-height: @nav_height;
+            padding: 0 10px;
+            transition: all .2s ease;
+            text-decoration: none;
             position: relative;
-        }
+            white-space: nowrap;
 
-        #site-nav-vue {
-            display: flex;
-            
-            .nav-item {
-                line-height: @nav_height;
-                padding: 0 10px;
-                transition: all .2s ease;
-                text-decoration: none;
-                position: relative;
-                white-space: nowrap;
-
-                &:hover {
-                    background-color: rgba(255, 255, 255, .2);
-
-                    .sub-nav {
-                        display: flex;
-                    }
-                }
+            &:hover {
+                background-color: rgba(255, 255, 255, .2);
 
                 .sub-nav {
-                    display: none;
-                    flex-direction: column;
-                    position: absolute;
-                    top: 100%;
-                    right: 0;
-                    background-color: rgba(0, 0, 0, .8);
+                    display: flex;
+                }
+            }
 
-                    .nav-item {
-                        line-height: 40px;
-                    }
+            .sub-nav {
+                display: none;
+                flex-direction: column;
+                position: absolute;
+                top: 100%;
+                right: 0;
+                background-color: rgba(0, 0, 0, .8);
+
+                .nav-item {
+                    line-height: 40px;
                 }
             }
         }
+    }
 
-        // --------------------------------------------- 脚本列表
+    // --------------------------------------------- 脚本列表
 
-        #user-script-list,
-        #user-deleted-script-list,
-        #browse-script-list {
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
-            grid-gap: 20px;
-            border: 0;
-            box-shadow: none;
+    #user-script-list,
+    #user-deleted-script-list,
+    #browse-script-list {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 20px;
+        border: 0;
+        box-shadow: none;
 
-            li {
-                border: 1px solid #bbb;
-                box-shadow: 0 0 5px #ddd;
-                border-radius: 5px;
-                padding: 10px;
-                position: relative;
+        li {
+            border: 1px solid #bbb;
+            box-shadow: 0 0 5px #ddd;
+            border-radius: 5px;
+            padding: 10px;
+            position: relative;
 
-                a.script-link {
-                    .ellipsis(2);
-                    height: calc(3em  + 8px);
-                    font-size: 16px;
-                    margin: 4px -10px 4px -14px;
-                    padding: 4px 10px;
-                    background: linear-gradient(#fff, #eee);
-                    border-left: 7px solid #800;
-                    box-shadow: inset 0 1px rgb(0 0 0 / 10%), inset 0 -1px rgb(0 0 0 / 10%);
+            a.script-link {
+                .ellipsis(2);
+                height: calc(3em  + 8px);
+                font-size: 16px;
+                margin: 4px -10px 4px -14px;
+                padding: 4px 10px;
+                background: linear-gradient(#fff, #eee);
+                border-left: 7px solid #800;
+                box-shadow: inset 0 1px rgb(0 0 0 / 10%), inset 0 -1px rgb(0 0 0 / 10%);
+            }
+
+            .script-description {
+                .ellipsis(3);
+                text-indent: 2em;
+                margin: 10px 0 10px;
+                height: 4.5em;
+                font-size: 14px;
+            }
+
+            .inline-script-stats {
+                padding-top: 10px;
+                border-top: 1px solid #ebebeb;
+
+                dt {
+                    width: 40%;
                 }
 
-                .script-description {
-                    .ellipsis(3);
-                    text-indent: 2em;
-                    margin: 10px 0 10px;
-                    height: 4.5em;
-                    font-size: 14px;
-                }
-
-                .inline-script-stats {
-                    padding-top: 10px;
-                    border-top: 1px solid #ebebeb;
-
-                    dt {
-                        width: 40%;
-                    }
-
-                    dd {
-                        width: 60%;
-                    }
+                dd {
+                    width: 60%;
                 }
             }
         }
-    `
+    }
+`
 
-    less.render(lessInput, lessOptions).then(output => {
-        // output.css = string of css
-        // output.map = string of sourcemap
-        // output.imports = array of string filenames of the imports referenced
+less.render(lessInput, lessOptions).then(output => {
+    // output.css = string of css
+    // output.map = string of sourcemap
+    // output.imports = array of string filenames of the imports referenced
 
-        GM_addStyle(output.css)
-    }, err => {
-        console.error(err)
-    })
+    GM_addStyle(output.css)
+}, err => {
+    console.error(err)
+})
+
+window.addEventListener('load', () => {
+    // 样式初始化
+    GM_addStyle(GM_getResourceText('normalize.css'))
 
     // 导航
     const navContainer = document.createElement('div')
