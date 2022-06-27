d8eac76f2a58e59654550421e897c622487ca74d
kiccer <1072907338@qq.com>
2022-06-27 17:43:03 +0800

    ✨ feat: 脚本列表页面卡片增加版本号、安装按钮

--- scripts/greasyfork_beautify.js
+++ scripts/greasyfork_beautify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greasyfork Beautify
 // @namespace    https://github.com/kiccer
-// @version      0.27.alpha
+// @version      0.28.alpha
 // @description  优化导航栏样式 / 脚本列表改为卡片布局 / 代码高亮(atom-one-dark + vscode 风格) 等……融入式美化，自然、优雅，没有突兀感，仿佛页面原本就是如此……（更多优化逐步完善中！）
 // @description:en  Optimize the navigation bar style / script list to card layout / code highlighting (atom-one-dark + vscode style), etc. Into the style of beautification, more natural, more elegant, no sense of abruptness, as if the page is originally so. (more optimization in progress!)
 // @author       kiccer<1072907338@qq.com>
@@ -20,6 +20,7 @@
 // @grant        GM_info
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 /* globals $ less Vue hljs */
@@ -376,6 +377,16 @@ const lessInput = `
                     width: 60%;
                 }
             }
+
+            .install-link {
+                float: right;
+                font-size: 12px;
+
+                &:hover {
+                    transition: box-shadow .2s;
+                    box-shadow: 0 8px 16px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%);
+                }
+            }
         }
     }
 `
@@ -409,6 +420,163 @@ if (/https:\/\/greasyfork\.org\/[a-zA-Z-]+\/scripts\/\d{6}-.+\/code/.test(locati
     `)
 }
 
+// GreasyFork 的源代码，直接拷过来
+function getTampermonkey () {
+    return window.external?.Tampermonkey
+}
+
+function getViolentmonkey () {
+    return window.external?.Violentmonkey
+}
+
+function getInstalledVersion (name, namespace) {
+    return new Promise(function (resolve, reject) {
+        const tm = getTampermonkey()
+        if (tm) {
+            tm.isInstalled(name, namespace, function (i) {
+                if (i.installed) {
+                    resolve(i.version)
+                } else {
+                    resolve(null)
+                }
+            })
+            return
+        }
+
+        const vm = getViolentmonkey()
+        if (vm) {
+            vm.isInstalled(name, namespace).then(resolve)
+            return
+        };
+
+        reject(Error)
+    })
+}
+
+// https://developer.mozilla.org/en/docs/Toolkit_version_format
+function compareVersions (a, b) {
+    if (a === b) {
+        return 0
+    }
+    const aParts = a.split('.')
+    const bParts = b.split('.')
+    for (let i = 0; i < aParts.length; i++) {
+        const result = compareVersionPart(aParts[i], bParts[i])
+        if (result !== 0) {
+            return result
+        }
+    }
+    return 0
+}
+
+function compareVersionPart (partA, partB) {
+    const partAParts = parseVersionPart(partA)
+    const partBParts = parseVersionPart(partB)
+    for (let i = 0; i < partAParts.length; i++) {
+        // "A string-part that exists is always less than a string-part that doesn't exist"
+        if (partAParts[i].length > 0 && partBParts[i].length === 0) {
+            return -1
+        }
+        if (partAParts[i].length === 0 && partBParts[i].length > 0) {
+            return 1
+        }
+        if (partAParts[i] > partBParts[i]) {
+            return 1
+        }
+        if (partAParts[i] < partBParts[i]) {
+            return -1
+        }
+    }
+    return 0
+}
+
+// It goes number, string, number, string. If it doesn't exist, then
+// 0 for numbers, empty string for strings.
+function parseVersionPart (part) {
+    if (!part) {
+        return [0, '', 0, '']
+    }
+    const partParts = /([0-9]*)([^0-9]*)([0-9]*)([^0-9]*)/.exec(part)
+    return [
+        partParts[1] ? parseInt(partParts[1]) : 0,
+        partParts[2],
+        partParts[3] ? parseInt(partParts[3]) : 0,
+        partParts[4]
+    ]
+}
+
+function handleInstallResult (installButton, installedVersion, version) {
+    if (installedVersion == null) {
+        // Not installed, do nothing
+        return
+    }
+
+    installButton.removeAttribute('data-ping-url')
+
+    switch (compareVersions(installedVersion, version)) {
+    // Upgrade
+    case -1:
+        installButton.textContent = installButton.getAttribute('data-update-label')
+        break
+        // Downgrade
+    case 1:
+        installButton.textContent = installButton.getAttribute('data-downgrade-label')
+        break
+        // Equal
+    case 0:
+        installButton.textContent = installButton.getAttribute('data-reinstall-label')
+        break
+    }
+}
+
+function checkForUpdatesJS (installButton, retry) {
+    const name = installButton.getAttribute('data-script-name')
+    const namespace = installButton.getAttribute('data-script-namespace')
+    const version = installButton.getAttribute('data-script-version')
+
+    getInstalledVersion(name, namespace).then(function (installedVersion) {
+        handleInstallResult(installButton, installedVersion, version)
+    }, function () {
+        if (retry) {
+            setTimeout(function () { checkForUpdatesJS(installButton, false) }, 1000)
+        }
+    })
+}
+
+// function checkForUpdatesCSS (installButton) {
+//     const name = installButton.getAttribute('data-script-name')
+//     const namespace = installButton.getAttribute('data-script-namespace')
+//     postMessage({ type: 'style-version-query', name, namespace, url: location.href }, location.origin)
+// }
+
+// Response from Stylus
+// window.addEventListener('message', function (event) {
+//     if (event.origin !== 'https://greasyfork.org' && event.origin !== 'https://sleazyfork.org') { return }
+
+//     if (event.data.type !== 'style-version') { return }
+
+//     const installButton = document.querySelector('.install-link[data-install-format=css]')
+//     if (installButton == null) { return }
+
+//     const version = installButton.getAttribute('data-script-version')
+
+//     const installedVersion = event.data.version
+
+//     handleInstallResult(installButton, installedVersion, version)
+// }, false)
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const installButtonJS = document.querySelector('.install-link[data-install-format=js]')
+//     if (installButtonJS) {
+//         checkForUpdatesJS(installButtonJS, true)
+//     }
+//     const installButtonCSS = document.querySelector('.install-link[data-install-format=css]')
+//     if (installButtonCSS) {
+//         checkForUpdatesCSS(installButtonCSS)
+//     }
+// })
+
+// 页面加载完成后执行
 $(() => {
     // 导航
     const navContainer = document.createElement('div')
@@ -528,6 +696,41 @@ $(() => {
         $(pre).find('code').html(
             htmlSplit.map((n, i) => `<span class="marker" style="width: calc(${String(totalLines).length * 0.5}em + 20px);">${i + 1}</span>${n}`).join('\n')
         )
+    })
+
+    // 脚本列表页面，卡片
+    $(`
+        #user-script-list li[data-script-id],
+        #user-deleted-script-list li[data-script-id],
+        #browse-script-list li[data-script-id]
+    `).each((i, n) => {
+        const card = $(n)
+        const href = card.find('> article a.script-link').attr('href')
+
+        // TODO 显示脚本图标 (看情况，如果加了图标不好布局就算了)
+
+        $.ajax({
+            type: 'get',
+            url: href,
+            success: res => {
+                const html = $(res)
+
+                // 版本
+                card.find('.inline-script-stats').append(
+                    html.find('.script-show-version')
+                )
+
+                // 下载按钮
+                card.append(
+                    html.find('#install-area .install-link').eq(0)
+                )
+
+                // 下载按钮文案根据已安装的版本号调整
+                setTimeout(() => {
+                    checkForUpdatesJS(card.find('.install-link')[0], true)
+                })
+            }
+        })
     })
 
     // 脚本列表页面
