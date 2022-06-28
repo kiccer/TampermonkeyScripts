f49a32b32f23bb2524c391eafc13333c6211af7a
kiccer <1072907338@qq.com>
2022-06-28 09:19:42 +0800

    ✨ feat: 将版本检查代码提取到库，并从库拉取代码

---
+++ libs/greasyfork_check_version.js
@@ -0,0 +1,179 @@
+// ==UserScript==
+// @name         checkVersion
+// @description  Greasyfork Check Version Source Code
+// @version      1.0
+// @author       kiccer<1072907338@qq.com>
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    function getTampermonkey () {
+        return window.external?.Tampermonkey
+    }
+
+    function getViolentmonkey () {
+        return window.external?.Violentmonkey
+    }
+
+    function getInstalledVersion (name, namespace) {
+        return new Promise(function (resolve, reject) {
+            const tm = getTampermonkey()
+            if (tm) {
+                tm.isInstalled(name, namespace, function (i) {
+                    if (i.installed) {
+                        resolve(i.version)
+                    } else {
+                        resolve(null)
+                    }
+                })
+                return
+            }
+
+            const vm = getViolentmonkey()
+            if (vm) {
+                vm.isInstalled(name, namespace).then(resolve)
+                return
+            };
+
+            reject(Error)
+        })
+    }
+
+    // https://developer.mozilla.org/en/docs/Toolkit_version_format
+    function compareVersions (a, b) {
+        if (a === b) {
+            return 0
+        }
+        const aParts = a.split('.')
+        const bParts = b.split('.')
+        for (let i = 0; i < aParts.length; i++) {
+            const result = compareVersionPart(aParts[i], bParts[i])
+            if (result !== 0) {
+                return result
+            }
+        }
+        return 0
+    }
+
+    function compareVersionPart (partA, partB) {
+        const partAParts = parseVersionPart(partA)
+        const partBParts = parseVersionPart(partB)
+        for (let i = 0; i < partAParts.length; i++) {
+            // "A string-part that exists is always less than a string-part that doesn't exist"
+            if (partAParts[i].length > 0 && partBParts[i].length === 0) {
+                return -1
+            }
+            if (partAParts[i].length === 0 && partBParts[i].length > 0) {
+                return 1
+            }
+            if (partAParts[i] > partBParts[i]) {
+                return 1
+            }
+            if (partAParts[i] < partBParts[i]) {
+                return -1
+            }
+        }
+        return 0
+    }
+
+    // It goes number, string, number, string. If it doesn't exist, then
+    // 0 for numbers, empty string for strings.
+    function parseVersionPart (part) {
+        if (!part) {
+            return [0, '', 0, '']
+        }
+        const partParts = /([0-9]*)([^0-9]*)([0-9]*)([^0-9]*)/.exec(part)
+        return [
+            partParts[1] ? parseInt(partParts[1]) : 0,
+            partParts[2],
+            partParts[3] ? parseInt(partParts[3]) : 0,
+            partParts[4]
+        ]
+    }
+
+    function handleInstallResult (installButton, installedVersion, version) {
+        if (installedVersion == null) {
+            // Not installed, do nothing
+            return
+        }
+
+        installButton.removeAttribute('data-ping-url')
+
+        switch (compareVersions(installedVersion, version)) {
+        // Upgrade
+        case -1:
+            installButton.textContent = installButton.getAttribute('data-update-label')
+            break
+            // Downgrade
+        case 1:
+            installButton.textContent = installButton.getAttribute('data-downgrade-label')
+            break
+            // Equal
+        case 0:
+            installButton.textContent = installButton.getAttribute('data-reinstall-label')
+            break
+        }
+    }
+
+    function checkForUpdatesJS (installButton, retry) {
+        const name = installButton.getAttribute('data-script-name')
+        const namespace = installButton.getAttribute('data-script-namespace')
+        const version = installButton.getAttribute('data-script-version')
+
+        getInstalledVersion(name, namespace).then(function (installedVersion) {
+            handleInstallResult(installButton, installedVersion, version)
+        }, function () {
+            if (retry) {
+                setTimeout(function () { checkForUpdatesJS(installButton, false) }, 1000)
+            }
+        })
+    }
+
+    function checkForUpdatesCSS (installButton) {
+        const name = installButton.getAttribute('data-script-name')
+        const namespace = installButton.getAttribute('data-script-namespace')
+        postMessage({ type: 'style-version-query', name, namespace, url: location.href }, location.origin)
+    }
+
+    // Response from Stylus
+    // window.addEventListener('message', function (event) {
+    //     if (event.origin !== 'https://greasyfork.org' && event.origin !== 'https://sleazyfork.org') { return }
+
+    //     if (event.data.type !== 'style-version') { return }
+
+    //     const installButton = document.querySelector('.install-link[data-install-format=css]')
+    //     if (installButton == null) { return }
+
+    //     const version = installButton.getAttribute('data-script-version')
+
+    //     const installedVersion = event.data.version
+
+    //     handleInstallResult(installButton, installedVersion, version)
+    // }, false)
+
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     const installButtonJS = document.querySelector('.install-link[data-install-format=js]')
+    //     if (installButtonJS) {
+    //         checkForUpdatesJS(installButtonJS, true)
+    //     }
+    //     const installButtonCSS = document.querySelector('.install-link[data-install-format=css]')
+    //     if (installButtonCSS) {
+    //         checkForUpdatesCSS(installButtonCSS)
+    //     }
+    // })
+
+    if (!window.checkVersion) {
+        window.checkVersion = {
+            getTampermonkey,
+            getViolentmonkey,
+            getInstalledVersion,
+            compareVersions,
+            compareVersionPart,
+            parseVersionPart,
+            handleInstallResult,
+            checkForUpdatesJS,
+            checkForUpdatesCSS
+        }
+    }
+})()
--- scripts/greasyfork_beautify.js
+++ scripts/greasyfork_beautify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greasyfork Beautify
 // @namespace    https://github.com/kiccer
-// @version      1.2
+// @version      1.2.1
 // @description  优化导航栏样式 / 脚本列表改为卡片布局 / 代码高亮(atom-one-dark + vscode 风格) 等……融入式美化，自然、优雅，没有突兀感，仿佛页面原本就是如此……（更多优化逐步完善中！）
 // @description:en  Optimize the navigation bar style / script list to card layout / code highlighting (atom-one-dark + vscode style), etc. Into the style of beautification, more natural, more elegant, no sense of abruptness, as if the page is originally so. (more optimization in progress!)
 // @author       kiccer<1072907338@qq.com>
@@ -14,6 +14,7 @@
 // @require      https://cdn.bootcdn.net/ajax/libs/less.js/4.1.3/less.min.js
 // @require      https://cdn.bootcdn.net/ajax/libs/highlight.js/11.5.1/highlight.min.js
 // @require      https://cdn.bootcdn.net/ajax/libs/highlight.js/11.5.1/languages/javascript.min.js
+// @require      https://greasyfork.org/scripts/447149-checkversion/code/checkVersion.js?version=1065242
 // @resource normalize.css https://cdn.bootcdn.net/ajax/libs/normalize/8.0.1/normalize.min.css
 // @resource atom-one-dark.css https://cdn.bootcdn.net/ajax/libs/highlight.js/11.5.1/styles/atom-one-dark.min.css
 // @run-at       document-start
@@ -22,7 +23,7 @@
 // @grant        GM_getResourceText
 // ==/UserScript==
 
-/* globals $ less Vue hljs */
+/* globals $ less Vue hljs checkVersion */
 
 const VERSION = GM_info.script.version
 
@@ -469,170 +470,6 @@ if (/https:\/\/greasyfork\.org\/[a-zA-Z-]+\/scripts\/\d{6}-.+\/code/.test(locati
     `)
 }
 
-// GreasyFork 的源代码，直接拷过来
-const {
-    checkForUpdatesJS
-} = (() => {
-    function getTampermonkey () {
-        return window.external?.Tampermonkey
-    }
-
-    function getViolentmonkey () {
-        return window.external?.Violentmonkey
-    }
-
-    function getInstalledVersion (name, namespace) {
-        return new Promise(function (resolve, reject) {
-            const tm = getTampermonkey()
-            if (tm) {
-                tm.isInstalled(name, namespace, function (i) {
-                    if (i.installed) {
-                        resolve(i.version)
-                    } else {
-                        resolve(null)
-                    }
-                })
-                return
-            }
-
-            const vm = getViolentmonkey()
-            if (vm) {
-                vm.isInstalled(name, namespace).then(resolve)
-                return
-            };
-
-            reject(Error)
-        })
-    }
-
-    // https://developer.mozilla.org/en/docs/Toolkit_version_format
-    function compareVersions (a, b) {
-        if (a === b) {
-            return 0
-        }
-        const aParts = a.split('.')
-        const bParts = b.split('.')
-        for (let i = 0; i < aParts.length; i++) {
-            const result = compareVersionPart(aParts[i], bParts[i])
-            if (result !== 0) {
-                return result
-            }
-        }
-        return 0
-    }
-
-    function compareVersionPart (partA, partB) {
-        const partAParts = parseVersionPart(partA)
-        const partBParts = parseVersionPart(partB)
-        for (let i = 0; i < partAParts.length; i++) {
-            // "A string-part that exists is always less than a string-part that doesn't exist"
-            if (partAParts[i].length > 0 && partBParts[i].length === 0) {
-                return -1
-            }
-            if (partAParts[i].length === 0 && partBParts[i].length > 0) {
-                return 1
-            }
-            if (partAParts[i] > partBParts[i]) {
-                return 1
-            }
-            if (partAParts[i] < partBParts[i]) {
-                return -1
-            }
-        }
-        return 0
-    }
-
-    // It goes number, string, number, string. If it doesn't exist, then
-    // 0 for numbers, empty string for strings.
-    function parseVersionPart (part) {
-        if (!part) {
-            return [0, '', 0, '']
-        }
-        const partParts = /([0-9]*)([^0-9]*)([0-9]*)([^0-9]*)/.exec(part)
-        return [
-            partParts[1] ? parseInt(partParts[1]) : 0,
-            partParts[2],
-            partParts[3] ? parseInt(partParts[3]) : 0,
-            partParts[4]
-        ]
-    }
-
-    function handleInstallResult (installButton, installedVersion, version) {
-        if (installedVersion == null) {
-            // Not installed, do nothing
-            return
-        }
-
-        installButton.removeAttribute('data-ping-url')
-
-        switch (compareVersions(installedVersion, version)) {
-        // Upgrade
-        case -1:
-            installButton.textContent = installButton.getAttribute('data-update-label')
-            break
-            // Downgrade
-        case 1:
-            installButton.textContent = installButton.getAttribute('data-downgrade-label')
-            break
-            // Equal
-        case 0:
-            installButton.textContent = installButton.getAttribute('data-reinstall-label')
-            break
-        }
-    }
-
-    function checkForUpdatesJS (installButton, retry) {
-        const name = installButton.getAttribute('data-script-name')
-        const namespace = installButton.getAttribute('data-script-namespace')
-        const version = installButton.getAttribute('data-script-version')
-
-        getInstalledVersion(name, namespace).then(function (installedVersion) {
-            handleInstallResult(installButton, installedVersion, version)
-        }, function () {
-            if (retry) {
-                setTimeout(function () { checkForUpdatesJS(installButton, false) }, 1000)
-            }
-        })
-    }
-
-    // function checkForUpdatesCSS (installButton) {
-    //     const name = installButton.getAttribute('data-script-name')
-    //     const namespace = installButton.getAttribute('data-script-namespace')
-    //     postMessage({ type: 'style-version-query', name, namespace, url: location.href }, location.origin)
-    // }
-
-    // Response from Stylus
-    // window.addEventListener('message', function (event) {
-    //     if (event.origin !== 'https://greasyfork.org' && event.origin !== 'https://sleazyfork.org') { return }
-
-    //     if (event.data.type !== 'style-version') { return }
-
-    //     const installButton = document.querySelector('.install-link[data-install-format=css]')
-    //     if (installButton == null) { return }
-
-    //     const version = installButton.getAttribute('data-script-version')
-
-    //     const installedVersion = event.data.version
-
-    //     handleInstallResult(installButton, installedVersion, version)
-    // }, false)
-
-    // document.addEventListener('DOMContentLoaded', function () {
-    //     const installButtonJS = document.querySelector('.install-link[data-install-format=js]')
-    //     if (installButtonJS) {
-    //         checkForUpdatesJS(installButtonJS, true)
-    //     }
-    //     const installButtonCSS = document.querySelector('.install-link[data-install-format=css]')
-    //     if (installButtonCSS) {
-    //         checkForUpdatesCSS(installButtonCSS)
-    //     }
-    // })
-
-    return {
-        checkForUpdatesJS
-    }
-})()
-
 // 页面加载完成后执行
 $(() => {
     // 导航
@@ -799,7 +636,7 @@ $(() => {
 
                 // 下载按钮文案根据已安装的版本号调整
                 setTimeout(() => {
-                    checkForUpdatesJS(card.find('.install-link-copy')[0], true)
+                    checkVersion.checkForUpdatesJS(card.find('.install-link-copy')[0], true)
                 })
             }
         })
