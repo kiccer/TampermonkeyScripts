0b6329d5defa756786a76472a5fb803b69553743
kiccer <1072907338@qq.com>
2022-06-29 15:32:03 +0800

    🐞 fix: 修复版本判断类型错误

--- scripts/greasyfork_beautify.js
+++ scripts/greasyfork_beautify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greasyfork Beautify
 // @namespace    https://github.com/kiccer
-// @version      1.4.1
+// @version      1.4.2
 // @description  优化导航栏样式 / 脚本列表改为卡片布局 / 代码高亮(atom-one-dark + vscode 风格) 等……融入式美化，自然、优雅，没有突兀感，仿佛页面原本就是如此……（更多优化逐步完善中！）
 // @description:en  Optimize the navigation bar style / script list to card layout / code highlighting (atom-one-dark + vscode style), etc. Into the style of beautification, more natural, more elegant, no sense of abruptness, as if the page is originally so. (more optimization in progress!)
 // @author       kiccer<1072907338@qq.com>
@@ -636,7 +636,7 @@ $(() => {
                         <a
                             class="has-new-version"
                             href="https://greasyfork.org/scripts/446849-greasyfork-beautify/code/Greasyfork%20Beautify.user.js"
-                            v-if="lastVersion !== ${VERSION}"
+                            v-if="lastVersion !== '${VERSION}'"
                         >Update to V{{ lastVersion }}</a>
                     </div>
 
@@ -655,8 +655,7 @@ $(() => {
 
         data () {
             return {
-                lastVersion: false,
-                lastVersionInstallUrl: '',
+                lastVersion: VERSION,
                 dom: $('#nav-user-info .user-profile-link a, #nav-user-info .sign-in-link a'),
                 logoutDom: $('.sign-out-link a'),
                 isLogin: $('.sign-out-link').length > 0 // 存在登出按钮则表示已登录
@@ -674,7 +673,6 @@ $(() => {
                     success: res => {
                         const html = $(res)
                         this.lastVersion = html.find('dd.script-show-version span').text()
-                        this.lastVersionInstallUrl = html.find('#install-area .install-link:eq(0)').attr('href')
                     }
                 })
             }
