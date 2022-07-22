00af48b6a3954b70bf846508b31c04ad97455dd7
kiccer <1072907338@qq.com>
2022-07-22 14:37:32 +0800

    🐞 fix: 解决当脚本ID位数不为6位时，出现的样式错误

--- scripts/greasyfork_beautify.js
+++ scripts/greasyfork_beautify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greasyfork Beautify
 // @namespace    https://github.com/kiccer
-// @version      1.6
+// @version      1.6.1
 // @description  优化导航栏样式 / 脚本列表改为卡片布局 / 代码高亮(atom-one-dark + vscode 风格) 等……融入式美化，自然、优雅，没有突兀感，仿佛页面原本就是如此……（更多优化逐步完善中！）
 // @description:en  Optimize the navigation bar style / script list to card layout / code highlighting (atom-one-dark + vscode style), etc. Into the style of beautification, more natural, more elegant, no sense of abruptness, as if the page is originally so. (more optimization in progress!)
 // @author       kiccer<1072907338@qq.com>
@@ -494,7 +494,7 @@ less.render(lessInput, lessOptions).then(output => {
 })
 
 // 查看代码页面简化，隐藏信息
-if (/https:\/\/greasyfork\.org\/[a-zA-Z-]+\/scripts\/\d{6}-.+\/code/.test(location.href)) {
+if (/https:\/\/greasyfork\.org\/[a-zA-Z-]+\/scripts\/\d+-.+\/code/.test(location.href)) {
     GM_addStyle(`
         #script-info header,
         #install-area,
